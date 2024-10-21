62ca73324c945d5cf9bb6ab9384abff935acea36
TomkoSK <tomaslukac599@gmail.com>
2024-10-21 17:47:04 +0200

    notification is now shown when enabling and disabling program

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-Made for doing homework on https://app.alphie.app, for those parts where you have to drag letters to make a word, you press control on your keyboard to enable typing and it drags the letters you type into empty squares  
+Made for doing homework on https://app.alphie.app, for those parts where letters are dragged, control is pressed to enable typing and the letters typed on the keyboard are found on the screen and automatically dragged to empty spaces
   
 (Program is made for 1920x1080 screen, with the website at 100% zoom)
 
 ## TODO:
-make it search for letters from top to bottom  
-maybe make characters like á ý č etc work  
-make capital letters work  
-make screen state refresh more often  
+make pyautogui search for letters from top to bottom
+maybe make letters like á ý č etc work  
+make capital letters work
 
 ## DEPENDENCIES:
-PIL 9.0+  
+PIL 9.0+
 pyautogui  
 pynput
+notify2
+dbus-python
--- main.py
+++ main.py
@@ -2,18 +2,34 @@ from pynput import keyboard
 import pyautogui
 import collections
 import threading
+import time
+import notify2
 
 SCREENSHOT_X, SCREENSHOT_Y = [600, 200]
 
 active = False
 inputQueue = collections.deque()
 screenshot = None
+lastUpdateAt = time.time()
+notifier = notify2.init("Alphie Script")
 
 def updateScreen():
-    global screenshot, SCREENSHOT_X, SCREENSHOT_Y
-    screenshot = pyautogui.screenshot(region=(SCREENSHOT_X, SCREENSHOT_Y, 1100, 700))
+    global screenshot, SCREENSHOT_X, SCREENSHOT_Y, lastUpdateAt
+    screenshot = pyautogui.screenshot(region=(SCREENSHOT_X, SCREENSHOT_Y, 1100, 800))
+    lastUpdateAt = time.time()
 
 updateScreen()
+
+def notify(active):
+    global notifier
+    if(active):
+        n = notify2.Notification("ENABLED")
+        n.set_timeout(750)
+        n.show()
+    else:
+        n = notify2.Notification("DISABLED")
+        n.set_timeout(750)
+        n.show()
 
 def onPress(key):
     global active, inputQueue
@@ -24,11 +40,11 @@ def onPress(key):
             if(key == keyboard.Key.ctrl):
                 active = not active
                 updateScreen()
-                print("ACTIVATE")
+                notify(True)
     except AttributeError:
         if(key == keyboard.Key.ctrl):
             active = not active
-            print("DEACTIVATE")
+            notify(False)
 
 listener = keyboard.Listener(
     on_press=onPress)
@@ -44,4 +60,8 @@ while True:
             pyautogui.dragTo(x=squareLocation[0]+SCREENSHOT_X, y=squareLocation[1]+SCREENSHOT_Y)
         except pyautogui.ImageNotFoundException:
             pass
+        except FileNotFoundError:
+            print(f"[WARNING] images/{character}.png not found")
+        updateScreen()
+    if(time.time()-lastUpdateAt > 0.3 and active):
         updateScreen()
